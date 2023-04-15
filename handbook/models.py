b73b824179f344c49e52e4af7ad5813e85dbddaa
samargarg <samargarg54@gmail.com>
2023-04-16 04:01:45 +0530

    Added Name field to Handbook Section

--- handbook/models.py
+++ handbook/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class HandbookSection(models.Model):
     number = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=32, blank=True, null=True)
 
     def __str__(self):
         return f"{self.number}"
