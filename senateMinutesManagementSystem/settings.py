397a059531d4f302cac453e433010c4182b0f6b0
samargarg <samargarg54@gmail.com>
2023-04-16 19:59:52 +0530

    Not Heroku

--- senateMinutesManagementSystem/settings.py
+++ senateMinutesManagementSystem/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -162,7 +162,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 
-django_heroku.settings(locals)
+# django_heroku.settings(locals)
 
 # REST_FRAMEWORK = {
 #     # Use Django's standard `django.contrib.auth` permissions,
