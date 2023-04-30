a2beb978c06ca266125d9d299fbacbc900e3edcb
samargarg <samargarg54@gmail.com>
2023-05-01 02:32:59 +0530

    Added More Tests

--- handbook/tests.py
+++ handbook/tests.py
@@ -3,6 +3,10 @@ from rest_framework.test import APITestCase
 from .models import *
 from datetime import datetime, date, timedelta
 from rest_framework import permissions, status
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 TEST_SENATE_MEETING_NUMBER = 99
 TEST_SENATE_POINT_NUMBER = 99
@@ -11,7 +15,11 @@ TEST_HANDBOOK_POINT_NUMBER = 99
 
 
 class HandbookTest(APITestCase):
-    senatePointID = None
+    handbookPointID = None
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(username='tester', password='test')
+        self.client.force_authenticate(user=self.user)
 
     def testCreateNewHandbookSection(self):
         data = {
@@ -34,3 +42,33 @@ class HandbookTest(APITestCase):
         print(response.json())
         self.assertTrue(response.status_code == status.HTTP_201_CREATED)
         self.assertTrue("number" in response.json())
+        self.handbookPointID = int(response.json()['id'])
+
+    def testDeleteHandbookSection(self):
+        self.testCreateNewHandbookSection()
+        response = self.client.delete(f'/handbook/handbookSections/{TEST_HANDBOOK_SECTION_NUMBER}/')
+        print(response)
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+    def testDeleteHandbookPoint(self):
+        self.testCreateNewHandbookPoint()
+        response = self.client.delete(f'/handbook/handbookPoints/{self.handbookPointID}/')
+        print(response)
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+    def testReadHandbookSection(self):
+        response = self.client.get(f'/handbook/handbookSections/')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+
+    def testReadHandbookPoint(self):
+        response = self.client.get(f'/handbook/handbookPoints/')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+
+    def testGetHandbookPointsBySectionNumber(self):
+        self.testCreateNewHandbookPoint()
+        response = self.client.get(
+            f'/handbook/getHandbookPointsBySectionNumber/?handbookSection={TEST_HANDBOOK_SECTION_NUMBER}')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
--- senateMeeting/tests.py
+++ senateMeeting/tests.py
@@ -3,6 +3,9 @@ from rest_framework.test import APITestCase
 from .models import *
 from datetime import datetime, date, timedelta
 from rest_framework import permissions, status
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 TEST_SENATE_MEETING_NUMBER = 99
 TEST_SENATE_POINT_NUMBER = 99
@@ -10,6 +13,10 @@ TEST_SENATE_POINT_NUMBER = 99
 
 class SenateMeetingTest(APITestCase):
     senatePointID = None
+
+    def setUp(self):
+        self.user = User.objects.create_superuser(username='tester', password='test')
+        self.client.force_authenticate(user=self.user)
 
     def testCreateNewSenateMeeting(self):
         data = {
@@ -53,7 +60,7 @@ class SenateMeetingTest(APITestCase):
             "resolution": "Test Edit Resolution",
             "senateMeeting": TEST_SENATE_MEETING_NUMBER,
             "approveComplete": True,
-            "approved": True,
+            "approved": 2,
             "handbookPointNewText": "Test Edit Handbook Point Text",
             # "handbookPoint": "Test Edit Handbook Point"
         }
@@ -62,4 +69,38 @@ class SenateMeetingTest(APITestCase):
         self.assertTrue(response.status_code == status.HTTP_200_OK)
         self.assertTrue(response.json()["handbookPointNewText"] == "Test Edit Handbook Point Text")
 
-    # TODO: Publish Handbook Test
+    def testDeleteSenateMeeting(self):
+        self.testCreateNewSenateMeeting()
+        response = self.client.delete(f'/senateMeeting/senateMeetings/{TEST_SENATE_MEETING_NUMBER}/')
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+    def testDeleteSenatePoint(self):
+        self.testCreateNewSenatePoint()
+        response = self.client.delete(f'/senateMeeting/senatePoints/{self.senatePointID}/')
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+    def testReadSenateMeeting(self):
+        response = self.client.get(f'/senateMeeting/senateMeetings/')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+
+    def testReadSenatePoint(self):
+        response = self.client.get(f'/senateMeeting/senatePoints/')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+
+    def testGetSenatePointsByMeetingNumber(self):
+        self.testCreateNewSenateMeeting()
+        response = self.client.get(f'/senateMeeting/getSenatePointsByMeetingNumber/?senateMeeting={TEST_SENATE_MEETING_NUMBER}')
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
+
+    # TODO: Need to be connected to handbook test
+    def testPublishHandbook(self):
+        self.testCreateNewSenatePoint()
+        data = {
+            "senateMeeting": TEST_SENATE_MEETING_NUMBER,
+        }
+        response = self.client.post(f'/senateMeeting/publishHandbook/', data)
+        print(response.json())
+        self.assertTrue(response.status_code == status.HTTP_200_OK)
